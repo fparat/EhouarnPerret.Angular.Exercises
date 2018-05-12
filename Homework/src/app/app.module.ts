ef9c95c5da7f44a2622c6818e031c0f71ecdb7b3
Franck Parat <franck.parat@gmail.com>
2018-05-12 12:27:52 +0800

    02 - Databinding 101 -> all

--- Homework/src/app/app.module.ts
+++ Homework/src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { SuccessAlertComponent } from './success-alert/success-alert.component';
 import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { UsernameInputComponent } from './username-input/username-input.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SuccessAlertComponent,
-    WarningAlertComponent
+    WarningAlertComponent,
+    UsernameInputComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Homework/src/app/username-input/username-input.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-username-input',
+  templateUrl: './username-input.component.html',
+  styleUrls: ['./username-input.component.css']
+})
+export class UsernameInputComponent implements OnInit {
+
+  username = '';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onReset(): void {
+    this.username = '';
+  }
+
+}
